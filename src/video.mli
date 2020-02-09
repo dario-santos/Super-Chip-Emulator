6c867c1acc16792570ff49f47311c453e11ea728
Dário Santos <dariovfsantos@gmail.com>
2020-02-09 19:33:29 -0100

    Added video.mli

---
+++ src/video.mli
@@ -0,0 +1,12 @@
+
+(* -----------------------------
+    Name:    fontset
+    Descrpt: Array with the fontset
+----------------------------- *)
+val fontset: int Array
+
+val init: unit -> unit
+
+val draw: unit -> unit
+
+val clear: unit -> unit
